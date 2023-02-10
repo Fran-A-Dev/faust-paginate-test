96acadd33e2ab9d6b45fb919d74183273a47d402
Fran Agulto <francis.agulto@wpengine.com>
2023-02-10 13:50:29 -0600

    added lib, apollo, paginate

--- wp-templates/index.js
+++ wp-templates/index.js
@@ -1,13 +1,15 @@
-import category from './category';
-import tag from './tag';
-import frontPage from './front-page';
-import page from './page';
-import single from './single';
+import category from "./category";
+import tag from "./tag";
+import frontPage from "./front-page";
+import page from "./page";
+import single from "./single";
+import categoryFood from "./category-food";
 
 export default {
   category,
   tag,
-  'front-page': frontPage,
+  "front-page": frontPage,
+  "category-food": categoryFood,
   page,
   single,
 };
